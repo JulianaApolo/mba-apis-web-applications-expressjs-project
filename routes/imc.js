1beb3535fe4a650a7e104e0c849ed9e4753da3a7
Juliana Apolo <juliana.apolo@grupofolha.com.br>
2023-03-21 22:21:31 -0300

    Refatorando

--- routes/imc.js
+++ routes/imc.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function(req, res, next) {
-    const { height, weight } = req.body;
-    const imc = Number((weight / (height ** 2)).toFixed(2));
-    
-    let imcDescription = '';
+function calculateImc(height, weight) {
+    return Number((weight / (height ** 2)).toFixed(2));
+}
+
+function translateImc(imc) {
     if (imc < 18.5) imcDescription = "magreza";
     else if (imc <= 24.9) imcDescription = "normal";
     else if (imc <= 29.9) imcDescription = "sobrepeso";
-    else this.imcDescription = "obesidade";
+    else imcDescription = "obesidade";
+
+    return imcDescription;
+}
+
+router.post('/', function(req, res, next) {
+    const { height, weight } = req.body;
+    const imc = calculateImc();
+    const imcDescription = translateImc(imc);
+    
     
     res.send(JSON.stringify({'height': height, 'weight': weight, 'imc': imc, 'imcDescription': imcDescription}));
 });
